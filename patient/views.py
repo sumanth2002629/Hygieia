f2b7d643ef2b24e6a081d2cbe92bf345c4941a96
Sooraj Sathish <sooraj@Soorajs-MacBook-Air.local>
2022-06-25 12:25:29 +0530

    search based on specialisation in booking appoin. View slots not working yet

--- patient/views.py
+++ patient/views.py
@@ -1,4 +1,6 @@
+from time import thread_time
 from django.shortcuts import render
+from register_login.models import Doctor
 
 # Create your views here.
 
@@ -7,4 +9,14 @@ def home(request):
     return render(request, 'patient/home.html')
 
 def book_appoinment(request):
-    return render(request, 'patient/book_appoinment.html')
+    #first recieve specialisation and sort doctors based on that
+
+    d = Doctor.objects.all()
+    for i in d:
+        print(i.user.username,i.specialisation)
+
+    if "specialisation" in request.GET:
+        print("searching")
+        d=d.filter(specialisation = request.GET["specialisation"])
+
+    return render(request, 'patient/book_appoinment.html',{"available_doctors":d})
